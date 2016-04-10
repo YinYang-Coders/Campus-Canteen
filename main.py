7190aa262b206c7e80bfec3d4cd7a24ad6588a9d
sahilkhurana02 <sahil_khurana@outlook.com>
2016-04-10 06:14:25 +0530

    Linked login and register page

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, g, session, url_for, request, render_t
 from flask.ext.mail import Mail
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
-from wtforms import StringField, BooleanField, TextAreaField
+from wtforms import StringField, BooleanField, TextAreaField, PasswordField, validators
 from wtforms.validators import DataRequired, Length
 from flask.ext.login import login_user, logout_user, current_user, \
     login_required
@@ -24,20 +24,41 @@ class LoginForm(Form):
     user_name = StringField('user_name', validators=[DataRequired()])
     password = StringField('password', validators=[DataRequired()])
 
+class RegisterForm(Form):
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+
 #DATABASE
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	user_name = db.Column(db.String(64), index=True, unique=True)
 	password = StringField(db.String(64), index=True)
 
+db.create_all()
 
 
 
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login_page():
-	form = LoginForm()
+	form = LoginForm(csrf_enabled = False)
 	return render_template("login.html")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm(csrf_enabled = False)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 # Start development web server
 if __name__=='__main__':
