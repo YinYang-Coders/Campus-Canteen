1480e2817080041c20a23f15400768bcdd2cc73c
sahilkhurana02 <sahil_khurana@outlook.com>
2016-04-20 14:50:19 +0530

    implementing sqlite3

--- main.py
+++ main.py
@@ -1,65 +1,32 @@
 import os
-from flask.ext.wtf import Form
-from flask import Flask, render_template, g, session, url_for, request, render_template_string
-from flask.ext.mail import Mail
-from flask.ext.login import LoginManager
-from flask.ext.sqlalchemy import SQLAlchemy
-from wtforms import StringField, BooleanField, TextAreaField, PasswordField, validators
-from wtforms.validators import DataRequired, Length
-from flask.ext.login import login_user, logout_user, current_user, \
-    login_required
+import sqlite3
+from flask import Flask, render_template, flash, redirect, url_for, request, g
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
-db = SQLAlchemy(app)
 
 #CONFIG
 CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SECRET_KEY = 'secret'
 
-#FORMS
-class LoginForm(Form):
-    user_name = StringField('user_name', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
-
-class RegisterForm(Form):
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email Address', [validators.Length(min=6, max=35)])
-    password = PasswordField('New Password', [
-        validators.Required(),
-        validators.EqualTo('confirm', message='Passwords must match')
-    ])
-    confirm = PasswordField('Repeat Password')
-
-#DATABASE
-class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	user_name = db.Column(db.String(64), index=True, unique=True)
-	password = StringField(db.String(64), index=True)
-
-db.create_all()
-
-
+#Model
+db = sqlite3.connect('mydb')
+cursor = db.cursor()
+cursor.execute('''DROP TABLE users''')
+cursor.execute('''
+	CREATE TABLE users(id INTEGER PRIMARY KEY, name TEXT,
+						password TEXT)
+''')
+db.commit()
 
 @app.route('/')
-@app.route('/login', methods=['GET', 'POST'])
-def login_page():
-	form = LoginForm(csrf_enabled = False)
-	return render_template("login.html")
+@app.route('/login')
+def login():
+	return render_template('login.html')
 
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/register')
 def register():
-    form = RegisterForm(csrf_enabled = False)
-    if request.method == 'POST' and form.validate():
-        user = User(form.username.data, form.email.data,
-                    form.password.data)
-        db_session.add(user)
-        flash('Thanks for registering')
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
+	return render_template('register.html')
 
-# Start development web server
-if __name__=='__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+	app.run(debug = True)
