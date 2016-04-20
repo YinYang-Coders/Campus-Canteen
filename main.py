546211bd90ce795f545bf8738b89ceaa203ddcbc
sahilkhurana02 <sahil_khurana@outlook.com>
2016-04-20 19:46:18 +0530

    done with sqlite3. next stop SQLAlchemy

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 from flask import Flask, render_template, flash, redirect, url_for, request, g
-
+from wtforms import Form, BooleanField, StringField, validators
 
 app = Flask(__name__)
 
@@ -17,16 +17,26 @@ cursor.execute('''
 	CREATE TABLE users(id INTEGER PRIMARY KEY, name TEXT,
 						password TEXT)
 ''')
-db.commit()
 
-@app.route('/')
-@app.route('/login')
+#Forms
+class RegistrationForm(Form):
+	username = StringField('Username', [validators.InputRequired()])
+	password = StringField('Password', [validators.InputRequired()])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
-	return render_template('login.html')
+	return render_template("login.html")
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-	return render_template('register.html')
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.password.data)
+        db_session.add(user)
+        flash('Thanks for registering')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
 	app.run(debug = True)
