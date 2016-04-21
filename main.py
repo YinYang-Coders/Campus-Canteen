21f80519cf4184fa105e8cd5d88dee72f9e094a4
sahilkhurana02 <sahil_khurana@outlook.com>
2016-04-21 16:11:47 +0530

    started with SQLAlchemy, created database along with basic form

--- main.py
+++ main.py
@@ -2,21 +2,24 @@ import os
 import sqlite3
 from flask import Flask, render_template, flash, redirect, url_for, request, g
 from wtforms import Form, BooleanField, StringField, validators
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.wtf import Form
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 #CONFIG
 CSRF_ENABLED = True
 SECRET_KEY = 'secret'
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 #Model
-db = sqlite3.connect('mydb')
-cursor = db.cursor()
-cursor.execute('''DROP TABLE users''')
-cursor.execute('''
-	CREATE TABLE users(id INTEGER PRIMARY KEY, name TEXT,
-						password TEXT)
-''')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(255), index = True, unique = True)
+    password = db.Column(db.String(64), index = True)
 
 #Forms
 class RegistrationForm(Form):
@@ -30,13 +33,14 @@ def login():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegistrationForm(request.form)
-    if request.method == 'POST' and form.validate():
-        user = User(form.username.data, form.password.data)
-        db_session.add(user)
-        flash('Thanks for registering')
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
+    form = RegistrationForm(csrf_enabled = False)
+    if form.validate_on_submit():
+        return redirect('/hello')
+    return render_template('registerform.html', title = 'Sign Up', form = form)
+
+@app.route('/hello')
+def hello():
+    return "Welcome to Campus Canteen"
 
 if __name__ == '__main__':
 	app.run(debug = True)
